47cfdf71a9cab337f1ee56686e3454a87352d688
Nguyễn Thị Ngọc Ánh <91924991+15NgocAnh@users.noreply.github.com>
2022-06-03 08:06:26 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,26 +1,42 @@
 let loginForm = document.querySelector('.login-form');
+let hoctap = document.querySelector('.login-form-1');
+let sp = document.querySelector('.login-form-2');
+let image = document.querySelector('.login-form-3');
  
-  document.querySelector('.profile_user').onclick = () =>{
-    loginForm.classList.toggle('active');
- }
-
- let hoctap = document.querySelector('.login-form-1');
- 
-  document.querySelector('.study').onclick = () =>{
-    hoctap.classList.toggle('active');
- }
-
- let sp = document.querySelector('.login-form-2');
- 
- document.querySelector('.san-pham').onclick = () =>{
-   sp.classList.toggle('active');
+document.querySelector('.ca-nhan').onclick = () =>{
+  loginForm.classList.toggle('active');
+  hoctap.classList.remove('active');
+  sp.classList.remove('active');
+  image.classList.remove('active');
 }
  
-function close() {
-    loginForm.classList.remove('active')
+ 
+document.querySelector('.study').onclick = () =>{
+  hoctap.classList.toggle('active');
+  loginForm.classList.remove('active');
+  sp.classList.remove('active');
+  image.classList.remove('active');
 }
 
-loginForm.addEventListener('click',close)
-loginForm.addEventListener('click', function(event) {
-    event.stopPropagation()
-})
+
+document.querySelector('.san-pham').onclick = () =>{
+    sp.classList.toggle('active');
+    hoctap.classList.remove('active');
+    loginForm.classList.remove('active');
+    image.classList.remove('active');
+}
+
+
+document.querySelector('.allbum').onclick = () =>{
+    image.classList.toggle('active');
+    sp.classList.remove('active');
+    hoctap.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    sp.classList.remove('active');
+    hoctap.classList.remove('active');
+    loginForm.classList.remove('active');
+    image.classList.remove('active');
+}
